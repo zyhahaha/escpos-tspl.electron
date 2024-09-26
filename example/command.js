d059c4c6baca467c51938706d8abda4b5f8383e3
zhaoyang <zhaoyang@lyky.cn>
2024-09-26 09:15:20 +0800

    update

---
+++ example/command.js
@@ -0,0 +1,30 @@
+const {
+    queryUsbDevicePathFn,
+    ipcTsplBitmap,
+    ipcEscPosCommand
+} = require('../index.js')
+
+/**
+ * ESC/POS指令打印机
+ */
+const escposPrinterName = 'XP-80C'
+queryUsbDevicePathFn(escposPrinterName, usbDevicePath => {
+    const commands = [
+        0x1B, 0x40, // 初始化打印机  
+        0x1B, 0x56, 0x30, // 设置文本大小（这里假设是0，表示默认大小）  
+        0x1B, 0x21, 0x30, // 选择标准字符集（美国ASCII）  
+        0x1B, 0x61, 0x30, // 设置文本对齐（0表示左对齐）  
+        0x68, 0x65, 0x6C, 0x6C, 0x6F, // hello 文本（ASCII编码）  
+        0x0A, // LF换行符（在某些打印机上可能需要，但ESC/POS通常使用GS E进行换行）  
+        0x1B, 0x45, 0x32  // GS E 2 回车换行（更常用的ESC/POS换行方式）  
+    ]
+    ipcEscPosCommand(usbDevicePath, commands)
+})
+
+/**
+ * TSPL指令打印机
+ */
+// const tsplPrinterName = 'HPRT N41'
+// queryUsbDevicePathFn(tsplPrinterName, usbDevicePath => {
+//     ipcTsplBitmap(usbDevicePath, base64Data)
+// })
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const { queryUsbDevicePathFn } = require('./utils/usb.js')
 const { ipcTsplCommand, ipcTsplBitmap } = require('./utils/tspl/index.js')
-const { ipcEscPosBitmap } = require('./utils/escpos/index.js')
+const { ipcEscPosCommand, ipcEscPosBitmap } = require('./utils/escpos/index.js')
 
 module.exports = {
     queryUsbDevicePathFn,
     ipcTsplCommand,
     ipcTsplBitmap,
+    ipcEscPosCommand,
     ipcEscPosBitmap
 }
