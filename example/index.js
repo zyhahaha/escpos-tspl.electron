d7f60a4a78b167211632ccfb27c6d81d99f794c3
zhaoyang <zhaoyang@lyky.cn>
2024-09-27 15:21:42 +0800

    update

--- example/index.js
+++ example/index.js
@@ -2,14 +2,14 @@ const {
     queryUsbDevicePathFn,
     ipcTsplBitmap,
     ipcEscPosBitmap
-} = require('../index.js')
+} = require('../dist/index.js')
 
 const base64Data = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAAXNSR0IArs4c6QAAAF1JREFUKFNjZCASMDIwMIAwJwMDw3eoHhj7P7IZIEUgEMDAwHAPyhZkYGA4iG4RTCHIlCwGBoaXDAwMKxgYGP7gUggSN4RafwObs2EmguQMoApvElKI1//IJtJZIQDzWQwLlBenDAAAAABJRU5ErkJggg=='
 
 /**
  * ESC/POS指令打印机
  */
-const escposPrinterName = 'XP-80C'
+const escposPrinterName = 'XP-58 (副本 1)'
 queryUsbDevicePathFn(escposPrinterName, usbDevicePath => {
     ipcEscPosBitmap(usbDevicePath, base64Data)
 })
